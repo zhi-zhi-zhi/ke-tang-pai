503f49b16147ca2b7bacf62e2316d7bc90e97755
tq <116020907@qq.com>
2019-12-16 22:25:50 +0800

    first commit

---
+++ src/api/auth.js
@@ -0,0 +1,60 @@
+import api from './index';
+import { axios } from '@/utils/request';
+
+/**
+ * todo: 整个api这个模块，找时间用 TS 重构一下
+ * parameter: {
+ *   username: string,
+ *   password: string,
+ *   remember_me: true,
+ *   captcha: 'ab91',
+ * }
+ * @param parameter
+ * @returns {AxiosPromise}
+ */
+export function login(parameter) {
+  return axios({
+    url: api.auth.login,
+    method: 'post',
+    data: parameter,
+  });
+}
+
+/**
+ * 啥都不要，带个token就完事了
+ * parameter: {}
+ * @param parameter
+ * @returns {AxiosPromise}
+ */
+export function logout(parameter) {
+  return axios({
+    url: api.auth.logout,
+    method: 'post',
+    data: parameter,
+  });
+}
+
+/**
+ * 获取手机验证码
+ * parameter: {
+ *   phoneNumber: String
+ * }
+ * @param parameter
+ * @returns {AxiosPromise}
+ */
+export function getSmsCaptcha(parameter) {
+  return axios({
+    url: api.auth.sms,
+    method: 'ost',
+    data: parameter,
+  });
+}
+
+/**
+ * 获取图片验证码
+ */
+export function getImageCaptcha() {
+  return axios({
+    url: api.auth.sendImage
+  })
+}
